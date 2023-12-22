2596b2f9813e3adaeb033009f92e0a8f76f6cc4c
mattot-the-builder <mattot.builder@gmail.com>
2023-12-23 01:33:01 +0800

    Added if else control flow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Control Flow");
+
+    // if
+    let is_true: bool = true;
+
+    if is_true {
+        println!("Condition is true");
+    } else {
+        println!("Condition is false");
+    }
+
+    let number = 5;
+    if number < 7 {
+        println!("Number is less than 7");
+    } else {
+        println!("Number is greater than or equal to 7");
+    }
+
+    // using if in let statement
+    let condition = true;
+    let a = if condition { 5 } else { 6 }; // must return same type
+
+    println!("The value of a : {a}");
 }
