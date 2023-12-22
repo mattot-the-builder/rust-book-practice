ea2ac1c2f54b038d60609575f6a1d4fe7e78a3d9
mattot-the-builder <mattot.builder@gmail.com>
2023-12-23 01:20:24 +0800

    Added simple function and function with return value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
+
+    test_function();
+
+    let sum = sum_numbers(1,2);
+    println!("Sum: {sum}");
+
+    // printing function output directly without storing 
+    println!("Sum: {}", sum_numbers(2,3));
+}
+
+// simple function
+fn test_function() {
+    println!("Im from test_function() ");
+}
+
+// function with return value
+fn sum_numbers(x: i32, y: i32) -> i32 {
+    x + y // don't need ; for expression (return value)
 }
